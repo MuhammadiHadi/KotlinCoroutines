19275ad4f0b34decfdde4c9d11520f2ec11671ca
HYPER <meerhadikhan364@gmail.com>
2023-06-07 12:32:08 +0500

    first commit

--- app/src/main/java/com/example/kotlincoroutines/ui/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/kotlincoroutines/ui/Fragment/HomeFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import com.example.kotlincoroutines.databinding.FragmentHomeBinding
 import kotlinx.coroutines.*
 import kotlin.concurrent.thread
+import kotlin.math.log
 
 class HomeFragment : Fragment() {
     private var _binding : FragmentHomeBinding? = null
@@ -35,11 +36,26 @@ class HomeFragment : Fragment() {
             CoroutineScope(Dispatchers.Main).launch {
                 taskTwo()
             }
+         CoroutineScope(Dispatchers.Main).async {
+             getValue()
+             task()
+         }
 
         }
 
 
         return binding.root
+    }
+    suspend fun getValue(){
+     val job=   CoroutineScope(Dispatchers.IO).async {
+       val task=  async { newTask() }
+        val new= async { task() }
+
+         Log.d(TAG, "${ task.await().toString()},${ new.await().toString() }")
+        }
+        // job object we are using for if complete then run other statement
+
+
     }
 
     private fun increase() {
@@ -58,4 +74,15 @@ class HomeFragment : Fragment() {
         yield()
         Log.d(TAG,"end Task Two")
     }
+
+   suspend fun newTask() : Int {
+        delay(3000)
+       return 20
+
+    }
+    // async we are using for waiting same time
+     suspend fun task():Int{
+      delay(3000)
+        return 30
+     }
 }
